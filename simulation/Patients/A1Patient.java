5d35108db1b0f584759eb63bec7aed05e60f8068
Boris0318 <borisesgal@gmail.com>
2022-11-19 16:51:59 +0100

    Constructors for patients

--- simulation/Patients/A1Patient.java
+++ simulation/Patients/A1Patient.java
@@ -3,6 +3,10 @@ package simulation.Patients;
 public class A1Patient implements Patient {
     private int priority;
 
+    public A1Patient (int priority){
+        this.priority = priority;
+    }
+
     @Override
     public int getPriority() {
         return priority;
