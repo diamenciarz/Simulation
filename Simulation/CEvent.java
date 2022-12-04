2c48a63e7eb7873987d3052984c62825142fdbbe
Stan <stasostaszewski@gmail.com>
2022-12-04 18:05:18 +0100

    Formatting

--- Simulation/CEvent.java
+++ Simulation/CEvent.java
@@ -8,8 +8,7 @@ package Simulation;
 
 import Simulation.City.Location;
 
-public class CEvent
-{
+public class CEvent {
 	/** The object involved with the event */
 	private CProcess target;
 	/** The type of the event */
@@ -20,34 +19,33 @@ public class CEvent
 	private Location location;
 
 	/**
-	*	Constructor for objects
-	*	@param dl	The object that will process the event
-	*	@param tp	The type of the event
-	*	@param tme	The time on which the event will be executed
-	*/
-	public CEvent(CProcess dl,int tp, double tme)
-	{
-		target=dl;
-		type=tp;
-		executionTime=tme;
+	 * Constructor for objects
+	 * 
+	 * @param dl  The object that will process the event
+	 * @param tp  The type of the event
+	 * @param tme The time on which the event will be executed
+	 */
+	public CEvent(CProcess dl, int tp, double tme) {
+		target = dl;
+		type = tp;
+		executionTime = tme;
 		location = new Location();
 
 	}
-	
+
 	/**
-	*	Method to signal the target to execute an event
-	*/
-	public void execute()
-	{
-		target.execute(type,executionTime);
+	 * Method to signal the target to execute an event
+	 */
+	public void execute() {
+		target.execute(type, executionTime);
 	}
-	
+
 	/**
-	*	Method to ask the event at which time it will be executed
-	*	@return	The time at which the event will be executed
-	*/
-	public double getExecutionTime()
-	{
+	 * Method to ask the event at which time it will be executed
+	 * 
+	 * @return The time at which the event will be executed
+	 */
+	public double getExecutionTime() {
 		return executionTime;
 	}
 
--- Simulation/City/City.java
+++ Simulation/City/City.java
@@ -2,33 +2,41 @@ package Simulation.City;
 
 import java.util.ArrayList;
 
-
-
 public class City {
     /**
      * city.get(0) is for the hex with the hospical
      */
-    public static ArrayList<Hex> city;
+    private static ArrayList<Hex> hexMap;
     public static int CENTER_X = 0;
     public static int CENTER_Y = 0;
     protected static final int N_HEX = 6;
     private static final double APHOTEM = (Math.sqrt(3) * Hex.RADIUS) / 2;
+    private static boolean alreadyGeneratedCity = false;
 
-    public City(){
-        city = generateCity();
+    public City() {
+        hexMap = generateCity();
+        alreadyGeneratedCity = true;
+    }
+    
+    public static ArrayList<Hex> getHexMap(){
+        if (!alreadyGeneratedCity) {
+            hexMap = generateCity();
+            alreadyGeneratedCity = true;
+        }
+        return hexMap;
     }
 
-    private ArrayList<Hex> generateCity(){
+    private static ArrayList<Hex> generateCity() {
 
         ArrayList<Hex> hex = new ArrayList<>();
-        
+
         hex.add(new Hex(true, CENTER_X, CENTER_Y));
 
         for (int i = 0; i < N_HEX + 1; i++) {
-                double desfase = Math.PI / Hex.POINTS;
-                int x = (int) (CENTER_X + 2 * APHOTEM * Math.cos(i * 2 * Math.PI / Hex.POINTS + desfase));
-                int y = (int) (CENTER_Y + 2 * APHOTEM * Math.sin(i * 2 * Math.PI / Hex.POINTS + desfase));
-                hex.add(new Hex(false, x, y));
+            double desfase = Math.PI / Hex.POINTS;
+            int x = (int) (CENTER_X + 2 * APHOTEM * Math.cos(i * 2 * Math.PI / Hex.POINTS + desfase));
+            int y = (int) (CENTER_Y + 2 * APHOTEM * Math.sin(i * 2 * Math.PI / Hex.POINTS + desfase));
+            hex.add(new Hex(false, x, y));
         }
         return hex;
     }
